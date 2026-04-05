75245794ff93f5bc7d9950c7e316ba73a6b5c435
Kenneth L. James Jr. <kj@Kenneths-MacBook-Air.local>
2026-04-05 10:51:11 -0500

    Fundraising: rename donate button to Purchase Raffle Tickets

--- app/fundraising/page.tsx
+++ app/fundraising/page.tsx
@@ -248,7 +248,7 @@ export default function FundraisingPage() {
               className="text-[0.6rem] tracking-[0.3em] uppercase mb-6"
               style={{ color: "#8B0000", fontFamily: "var(--font-body)" }}
             >
-              Secure Online Donation via PayPal
+              Secure Online Purchase via PayPal
             </div>
             <a
               href="https://www.paypal.com/ncp/payment/YFDSAQ6GNCCS4"
@@ -257,7 +257,7 @@ export default function FundraisingPage() {
               className="btn-fire"
               style={{ fontSize: "1rem", padding: "1rem 2.5rem" }}
             >
-              Donate Now
+              Purchase Raffle Tickets
               <svg viewBox="0 0 20 20" className="w-4 h-4 fill-current">
                 <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
               </svg>
